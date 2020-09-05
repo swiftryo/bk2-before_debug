d167e5c37502803da65f26b59e94567869d14d47
ryo <68486607+swiftryo@users.noreply.github.com>
2020-09-05 16:22:59 +0900

    finish　Two months　application4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def follows
+  end
+  
+  def followers
+  end
+
+  
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books, except: [:new] do
-  	resources :book_comments, only: [:create, :destroy]
-  	resource :favorites, only: [:create, :destroy]
-  end
   root 'home#top'
   get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
+  resources :books, except: [:new] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  
+end
 end
